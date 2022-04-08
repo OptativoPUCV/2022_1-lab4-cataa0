c30b2631e9448bf1c0b0103ed55f19e4d4d95d79
cataa0 <>
2022-04-08 15:59:27 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -55,7 +55,7 @@ HashMap * createMap(long capacity) {
   HashMap *aux=(HashMap*)malloc(sizeof(HashMap));
   aux->buckets=(Pair**)malloc(sizeof(Pair*)*capacity);
   aux->capacity=capacity;
-  aux->size=NULL;
+  aux->size=0;
   aux->current=-1;
     return aux;
 }
