7226f1b2a3b74ee38ed483add08c30f6727f423b
cataa0 <>
2022-04-10 20:25:32 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -60,8 +60,8 @@ void enlarge(HashMap * map) {
   long aux=0;
   map->buckets=(Pair**)malloc(sizeof(Pair)*map->capacity);
   map->size=0;
-  while(auz < old_buckets->capacity){
-    if(map->size == old_buckets->size){
+  while(aux < old_buckets[aux]->capacity){
+    if(map->size == old_buckets[aux]->size){
       insertMap(map, old_buckets[aux]->key, old_buckets[aux]->value);
       map->size++;
     }  
